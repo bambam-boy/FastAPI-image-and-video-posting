88a92e0e0d9928b405a3ea185f3a5756c5c0f82e
bambam-boy <vittokushankuhibosheri@gmail.com>
2026-08-19 11:09:44 +0330

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import uvicorn
+import subprocess
 
 if __name__ == "__main__":
+    subprocess.Popen(["streamlit", "run", "frontend.py"])
     uvicorn.run("app.core.app:app", host="127.0.0.1", port=8000, reload=True)
